6f73adebcbbf72ed014723ed425698af7ba4134a
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-21 18:33:02 -0500

    added router to server, created cars router file/folder

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const CarsRouter = require('../cars/cars-router.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/cars', CarsRouter);
 
 server.get('/', (req, res) => {
     res.send('<h2>Database Schema Design Project </h2>')

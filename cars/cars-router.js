654be827b6470237122cf00b87db8b0a196d8d22
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-21 18:53:22 -0500

    get and post requests work

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,4 +4,32 @@ const db = require('../data/db-config.js')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.select('*').from('car')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(err => {
+            console.log('error getting cars', err)
+            res.status(500).json({ errorMessage: 'Error getting the cars' })
+        })
+})
+
+router.post('/', (req, res) => {
+    const carData = req.body;
+    db('car').insert(carData, 'id')
+        .then(ids => {
+            const id = ids[0]
+            return db('car').where({id}).first().select('id', 'VIN', 'make', 'model', 'mileage', 'transmission', 'status').then(newCar => {
+                res.status(201).json(newCar)
+            })
+        })
+        .catch(err => {
+            console.log('error posting new car', err)
+            res.status(500).json({ message: 'Failed to store car' })
+        })
+})
+
+
+
 module.exports = router;
